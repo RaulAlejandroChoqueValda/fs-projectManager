12a24e5c361082d20d82f81a55b182b06f75f58f
RaulAlejandroChoqueValda <raul.alejandro.choque.valda@gmail.com>
2026-09-16 21:37:41 -0400

    fix: importar React en el archivo de prueba unitaria

--- src/components/FormularioTarea.test.jsx
+++ src/components/FormularioTarea.test.jsx
@@ -1,4 +1,5 @@
 // src/components/FormularioTarea.test.jsx
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { describe, it, expect, vi } from 'vitest'
