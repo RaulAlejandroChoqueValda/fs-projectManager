52283a9c5c452399ce97324c286d8d174b6267c8
RaulAlejandroChoqueValda <raul.alejandro.choque.valda@gmail.com>
2026-09-16 21:40:30 -0400

    fix: ignorar regla de linter para importacion de React en test

--- src/components/FormularioTarea.test.jsx
+++ src/components/FormularioTarea.test.jsx
@@ -1,4 +1,5 @@
 // src/components/FormularioTarea.test.jsx
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
